13e30b568ae48ea7174c5909657ff3ca8e704b30
Kauan Silva <kauanmacedo338@gmail.com>
2026-04-06 18:29:51 -0300

    atividade2

---
+++ Atividade2.ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Runtime.InteropServices;
+
+class Conversor
+{
+    public decimal F;
+
+    public decimal resultado;
+
+    public void Fahrenheit()
+    {
+        Console.Write("Digite o Fahrenheit (F): ");
+        F = Convert.ToDecimal(Console.ReadLine());
+    }
+
+    public void Calculo()
+    {
+        resultado = (5m / 9m) * (F - 32);
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Conversor converter = new Conversor();
+
+        Console.Clear();
+        Console.WriteLine("================================================");
+        Console.WriteLine("Conversor de Fahrenheit (F) para Celsius (C)");
+        Console.WriteLine("================================================");
+
+        Console.WriteLine(" ");
+
+        converter.Fahrenheit();
+
+        converter.Calculo();
+
+        Console.WriteLine($"{converter.resultado:F2} Graus Celsius");
+    }
+}
+
+
