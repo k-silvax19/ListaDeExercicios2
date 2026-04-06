e71d8ee868e4fc4b840b3e1496c3dd5d4a9b6a80
Kauan Silva <kauanmacedo338@gmail.com>
2026-04-06 19:14:23 -0300

    Atividade4

---
+++ atividade4.ConsoleApp2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Net.Http.Headers;
+
+namespace atividade4.ConsoleApp2;
+
+class Calculadora
+{
+    public decimal raio;
+
+    public decimal altura;
+
+    public decimal pi;
+
+    public decimal baseDoCilindro;
+
+    public decimal resultado;
+
+    public void Raio()
+    {
+        Console.Write("Digite o Raio: ");
+        raio = Convert.ToDecimal(Console.ReadLine());
+
+    }
+
+    public void Altura()
+    {
+        Console.Write("Digite a Altura: ");
+        altura = Convert.ToDecimal(Console.ReadLine());
+    }
+
+    public void Pi()
+    {
+        pi = (decimal)Math.PI;
+    }
+
+    public decimal CalcularBase()
+    {
+        return (decimal)Math.PI * (decimal)Math.Pow((double)raio, 2);
+    }
+
+    public decimal CalcularVolume()
+    {
+        return CalcularBase() * altura;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculadora calc = new Calculadora();
+
+        Console.Clear();
+
+        Console.WriteLine("----------------------");
+        Console.WriteLine("Calculadora de Volume Do Cilindro");
+        Console.WriteLine("----------------------");
+
+        Console.WriteLine(" ");
+
+        calc.Raio();
+        calc.Altura();
+
+        decimal resultado = calc.CalcularVolume();
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine($"O resultado do volume e: {resultado:F2}");
+
+    }
+}
