a80b3cb0b485a5589f99aad061397d26676f94c9
Kauan Silva <kauanmacedo338@gmail.com>
2026-04-06 20:06:44 -0300

    Atividade6

---
+++ atividade6.ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace atividade6.ConsoleApp2;
+
+class Converter
+{
+    public decimal C;
+
+    public decimal F;
+
+    public void GrausC()
+    {
+        Console.Write("Digite o grau em celsius (°C): ");
+        C = Convert.ToDecimal(Console.ReadLine());
+    }
+
+    public void Calculo()
+    {
+        F = (9m / 5m) * C + 32;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Converter conversor = new Converter();
+
+        Console.Clear();
+        Console.WriteLine("================================");
+        Console.WriteLine("Conversor De Celsius para Fahrenheit");
+        Console.WriteLine("================================");
+
+        Console.WriteLine(" ");
+
+        conversor.GrausC();
+        conversor.Calculo();
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine($"{conversor.F:F2}°F");
+    }
+}
