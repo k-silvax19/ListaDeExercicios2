63f8a7d11dc0e8df5c9e57a6635fdc75706d295d
Kauan Silva <kauanmacedo338@gmail.com>
2026-04-06 19:53:26 -0300

    atividade5

---
+++ atividade5.ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace atividade5.ConsoleApp2;
+
+class Calculo
+{
+    public decimal raio;
+
+    public decimal volume;
+
+    public void Raio()
+    {
+        Console.Write("Digite o Raio: ");
+        raio = Convert.ToDecimal(Console.ReadLine());
+    }
+
+    public void CalculoDoVolume()
+    {
+        volume = (4m / 3m) * (decimal)Math.PI * (decimal)Math.Pow((double)raio, 3);
+    }
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculo cal = new Calculo();
+
+        Console.WriteLine("=============================");
+        Console.WriteLine("Calculadora de volume de uma esfera");
+        Console.WriteLine("=============================");
+
+        Console.WriteLine(" ");
+
+        cal.Raio();
+        cal.CalculoDoVolume();
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine($"O Volume é: {cal.volume:F2}");
+
+    }
+}
